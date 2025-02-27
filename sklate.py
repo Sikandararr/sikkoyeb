eafa71292ecf65d5113073e1f8f3bd2deb1dcd0b
Sikandararr <meghasingh.7866@gmail.com>
2025-02-27 19:51:28 +0530

    Update sklate.py

--- sklate.py
+++ sklate.py
@@ -172,7 +172,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./bgmi {ip} {port} {duration} {13} {600}"
+        command = f"./bgmi {ip} {port} {duration} {200} {5}"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
